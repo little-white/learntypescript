5eaa64a332191e5dc4bd95793c72ef1c90bdcc22
guoyaqi <1664216915@qq.com>
2018-04-09 03:15:41 +0800

    解决依赖包找不到的问题

--- mocha.js
+++ mocha.js
@@ -4,5 +4,5 @@ var shell = require('shelljs')
 
 module.exports =  function (args) {
   var testFile = args[0].slice(0, args[0].indexOf('.'))
-  return shell.exec('mocha -r ts-node/register test/'+ testFile +'.test.ts').code
+  return shell.exec('mocha -r ts-node/register '+ __dirname +'/test/'+ testFile +'.test.ts').code
 }
