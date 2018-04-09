40fb5988ed15233e7148c4e777834d37229c4135
guoyaqi <1664216915@qq.com>
2018-04-09 14:36:46 +0800

    字符练习支持跳转到对应的页面

--- string/index.js
+++ string/index.js
@@ -1,6 +1,13 @@
 var common = require('../common')
 var path = require('path');
+var open = require("open");
 
-exports.problem = common.problem(path.resolve(__dirname, 'problem.md'))
-exports.solution = common.problem(path.resolve(__dirname, 'solution.md'))
+exports.problem = function () {
+  open("https://github.com/little-white/learntypescript/blob/master/string/problem.md");
+  return common.problem(path.resolve(__dirname, 'problem.md'))
+}
+exports.solution = function () {
+  open("https://github.com/little-white/learntypescript/blob/master/string/problem.md");
+  return common.problem(path.resolve(__dirname, 'solution.md'))
+}
 exports.verify = common.verify
